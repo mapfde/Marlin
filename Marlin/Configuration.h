79c57c4760d020daaf577f4e21a24ece60a1ff9b
Magnus Pfeffer <magnuspfeffer@gmail.com>
2020-06-14 14:00:43 +0200

    less default jerk

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -799,8 +799,8 @@
  */
 #define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
-  #define DEFAULT_XJERK 10.0
-  #define DEFAULT_YJERK 10.0
+  #define DEFAULT_XJERK 7.0
+  #define DEFAULT_YJERK 7.0
   #define DEFAULT_ZJERK  0.3
 
   //#define TRAVEL_EXTRA_XYJERK 0.0     // Additional jerk allowance for all travel moves
